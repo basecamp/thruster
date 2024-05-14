803bb025f76e02959d1aacf97021ccd7fb012b8f
Kartikey Tanna <tannakartikey@gmail.com>
2024-05-14 17:36:41 +0530

    static file code

--- internal/proxy_handler.go
+++ internal/proxy_handler.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"path/filepath"
 )
 
 func NewProxyHandler(targetUrl *url.URL, badGatewayPage string) http.Handler {
@@ -19,60 +20,57 @@ func NewProxyHandler(targetUrl *url.URL, badGatewayPage string) http.Handler {
 		},
 		ErrorHandler: ProxyErrorHandler(badGatewayPage),
 		Transport:    createProxyTransport(),
+		Director:     CustomDirector(targetUrl, badGatewayPage),
 	}
+}
+
+func CustomDirector(targetUrl *url.URL, badGatewayPage string) func(req *http.Request) {
+    return func(req *http.Request) {
+        filePath := filepath.Join("public", req.URL.Path)
+        if _, err := os.Stat(filePath); err == nil {
+            http.ServeFile(req.Response, req, filePath)
+            return
+        }
+
+        req.URL.Scheme = targetUrl.Scheme
+        req.URL.Host = targetUrl.Host
+        req.Host = targetUrl.Host
+    }
 }
 
 func ProxyErrorHandler(badGatewayPage string) func(w http.ResponseWriter, r *http.Request, err error) {
-	content, err := os.ReadFile(badGatewayPage)
-	if err != nil {
-		slog.Debug("No custom 502 page found", "path", badGatewayPage)
-		content = nil
-	}
+    content, err := os.ReadFile(badGatewayPage)
+    if err != nil {
+        slog.Debug("No custom 502 page found", "path", badGatewayPage)
+        content = nil
+    }
 
-	return func(w http.ResponseWriter, r *http.Request, err error) {
-		slog.Info("Unable to proxy request", "path", r.URL.Path, "error", err)
+    return func(w http.ResponseWriter, r *http.Request, err error) {
+        slog.Info("Unable to proxy request", "path", r.URL.Path, "error", err)
 
-		if isRequestEntityTooLarge(err) {
-			w.WriteHeader(http.StatusRequestEntityTooLarge)
-			return
-		}
+        if isRequestEntityTooLarge(err) {
+            w.WriteHeader(http.StatusRequestEntityTooLarge)
+            return
+        }
 
-		if content != nil {
-			w.Header().Set("Content-Type", "text/html")
-			w.WriteHeader(http.StatusBadGateway)
-			w.Write(content)
-		} else {
-			w.WriteHeader(http.StatusBadGateway)
-		}
-	}
+        if content != nil {
+            w.Header().Set("Content-Type", "text/html")
+            w.WriteHeader(http.StatusBadGateway)
+            w.Write(content)
+        } else {
+            w.WriteHeader(http.StatusBadGateway)
+        }
+    }
 }
 
 func isRequestEntityTooLarge(err error) bool {
-	var maxBytesError *http.MaxBytesError
-	return errors.As(err, &maxBytesError)
+    var maxBytesError *http.MaxBytesError
+    return errors.As(err, &maxBytesError)
 }
 
 func createProxyTransport() *http.Transport {
-	// The default transport requests compressed responses even if the client
-	// didn't. If it receives a compressed response but the client wants
-	// uncompressed, the transport decompresses the response transparently.
-	//
-	// Although that seems helpful, it doesn't play well with X-Sendfile
-	// responses, as it may result in us being handed a reference to a file on
-	// disk that is already compressed, and we'd have to similarly decompress it
-	// before serving it to the client. This is wasteful, especially since there
-	// was probably an uncompressed version of it on disk already. It's also a bit
-	// fiddly to do on the fly without the ability to seek around in the
-	// uncompressed content.
-	//
-	// Compression between us and the upstream server is likely to be of limited
-	// use anyway, since we're only proxying from localhost. Given that fact --
-	// and the fact that most clients *will* request compressed responses anyway,
-	// which makes all of this moot -- our best option is to disable this
-	// on-the-fly compression.
+    transport := http.DefaultTransport.(*http.Transport).Clone()
+    transport.DisableCompression = true
 
-	transport := http.DefaultTransport.(*http.Transport).Clone()
-	transport.DisableCompression = true
-
-	return transport
+    return transport
 }
