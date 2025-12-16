6b05f45167dd57bab15a79be2b2f2c281e46f29f
Kevin McConnell <kevin@37signals.com>
2025-12-16 12:10:37 +0000

    Move compression logic into its own middleware

--- internal/handler.go
+++ internal/handler.go
@@ -4,8 +4,6 @@ import (
 	"log/slog"
 	"net/http"
 	"net/url"
-
-	"github.com/klauspost/compress/gzhttp"
 )
 
 type HandlerOptions struct {
@@ -29,35 +27,7 @@ func NewHandler(options HandlerOptions) http.Handler {
 	handler = NewRequestStartHandler(handler)
 
 	if options.gzipCompressionEnabled {
-		var wrapper func(http.Handler) http.HandlerFunc
-		var err error
-
-		if options.gzipCompressionJitter > 0 {
-			wrapper, err = gzhttp.NewWrapper(
-				gzhttp.MinSize(1024),
-				gzhttp.CompressionLevel(6),
-				gzhttp.RandomJitter(options.gzipCompressionJitter, 0, false),
-			)
-		} else {
-			wrapper, err = gzhttp.NewWrapper(
-				gzhttp.MinSize(1024),
-				gzhttp.CompressionLevel(6),
-			)
-		}
-
-		if err != nil {
-			// If we cannot create the wrapper with the requested configuration (including jitter),
-			// we must fail hard rather than silently downgrading security or performance.
-			panic("failed to create gzip wrapper: " + err.Error())
-		}
-
-		gzipHandler := wrapper(handler)
-
-		if options.gzipCompressionDisableOnAuth {
-			handler = NewCompressionGuardHandler(gzipHandler)
-		} else {
-			handler = gzipHandler
-		}
+		handler = NewCompressionHandler(options.gzipCompressionJitter, options.gzipCompressionDisableOnAuth, handler)
 	}
 
 	if options.maxRequestBody > 0 {
